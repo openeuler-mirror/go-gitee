ce9b882026f62f3659b2ed41d9a05b4840384e39
edisonxiang <xiang.edison@gmail.com>
2019-10-08 19:35:57 +0800

    init pull request event

--- gitee/hook_event_types.go
+++ gitee/hook_event_types.go
@@ -29,8 +29,35 @@ type IssueEvent struct {
 	Action *string `json:"action,omitempty"`
 }
 
+type RepoInfo struct {
+	Project    *Project `json:"project,omitempty"`
+	Repository *Project `json:"repository,omitempty"`
+}
+
 type PullRequestEvent struct {
-	Action *string `json:"action,omitempty"`
+	Action         *string          `json:"action,omitempty"`
+	PullRequest    *PullRequest     `json:"pull_request,omitempty"`
+	Number         int64            `json:"number,omitempty"`
+	IID            int64            `json:"iid,omitempty"`
+	Title          *string          `json:"title,omitempty"`
+	Body           *string          `json:"body,omitempty"`
+	State          *string          `json:"state,omitempty"`
+	MergeStatus    *string          `json:"merge_status,omitempty"`
+	MergeCommitSha *string          `json:"merge_commit_sha,omitempty"`
+	URL            *string          `json:"url,omitempty"`
+	SourceBranch   *string          `json:"source_branch,omitempty"`
+	SourceRepo     *RepoInfo        `json:"source_repo,omitempty"`
+	TargetBranch   *string          `json:"target_branch,omitempty"`
+	TargetRepo     *RepoInfo        `json:"target_repo,omitempty"`
+	Project        *Project         `json:"project,omitempty"`
+	Repository     *Project         `json:"repository,omitempty"`
+	Author         *User            `json:"author,omitempty"`
+	UpdatedBy      *User            `json:"updated_by,omitempty"`
+	Sender         *User            `json:"sender,omitempty"`
+	TargetUser     *User            `json:"target_user,omitempty"`
+	Enterprise     *EnterpriseBasic `json:"enterprise,omitempty"`
+	HookName       *string          `json:"hook_name,omitempty"`
+	Password       *string          `json:"password,omitempty"`
 }
 
 type TagPushEvent struct {
